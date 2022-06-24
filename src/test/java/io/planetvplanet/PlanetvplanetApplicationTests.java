9cd410619b3153f3e65ab7e72c042364104cc2cc
Kemal Zebari <zebar002@cougars.csusm.edu>
2022-06-24 16:57:15 -0700

    add inital project layout

---
+++ src/test/java/io/planetvplanet/PlanetvplanetApplicationTests.java
@@ -0,0 +1,13 @@
+package io.planetvplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetvplanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
