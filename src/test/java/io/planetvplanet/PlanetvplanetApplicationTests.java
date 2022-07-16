a8a017f464c4045af531d33c333cc381385998f0
Kemal Zebari <zebar002@cougars.csusm.edu>
2022-07-16 11:46:32 -0700

    test: Update Spring Boot Application test to pass

--- src/test/java/io/planetvplanet/PlanetvplanetApplicationTests.java
+++ src/test/java/io/planetvplanet/PlanetvplanetApplicationTests.java
@@ -1,13 +1,24 @@
 package io.planetvplanet;
 
+import io.planetvplanet.controller.SearchController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class PlanetvplanetApplicationTests {
+
+	@Autowired
+	private SearchController searchController;
 
 	@Test
 	void contextLoads() {
+		assertThat(searchController).isNotNull();
 	}
 
 }
