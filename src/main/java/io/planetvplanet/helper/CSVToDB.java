b5166e38049358b9df330ed32484d43b70bba070
Kemal Zebari <kemalzebra@gmail.com>
2022-12-31 15:48:14 -0800

    refactor: Add helper methods to CSVToDB to improve readability

--- src/main/java/io/planetvplanet/helper/CSVToDB.java
+++ src/main/java/io/planetvplanet/helper/CSVToDB.java
@@ -2,56 +2,58 @@ package io.planetvplanet.helper;
 
 import io.planetvplanet.model.Planet;
 import io.planetvplanet.repository.PlanetRepository;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-
 public class CSVToDB {
-    /**
-     * Converts NASA's Planetary Systems Composite Data Table (a CSV file) into
-     * records in a database.
-     * @param repo: the planet repository bean
-     * @param filePath: file path to the planet data table
-     * @return list of planet entities
-     */
-    public static void convert(PlanetRepository repo, String filePath) throws IOException {
-        Reader reader = new FileReader(filePath);
-        CSVFormat csvFormatter = CSVFormat
-                .DEFAULT
-                .builder()
-                .setCommentMarker('#')
-                .setHeader()
-                .build();
-        CSVParser csvParser = CSVParser.parse(reader, csvFormatter);
+  /**
+   * Converts NASA's Planetary Systems Composite Data Table (a CSV file) into
+   * records in a database.
+   * @param repo: the planet repository bean
+   * @param filePath: file path to the planet data table
+   * @return list of planet entities
+   */
+  public static void convert(PlanetRepository repo, String filePath) throws IOException {
+    Reader reader = new FileReader(filePath);
+    CSVFormat csvFormatter = CSVFormat.DEFAULT.builder().setCommentMarker('#').setHeader().build();
+    CSVParser csvParser = CSVParser.parse(reader, csvFormatter);
 
-        for(CSVRecord record : csvParser) {
-            String planetName = record.get("pl_name");
-            String hostName = record.get("hostname");
-            String discoveryYear = record.get("disc_year");
-            String discoveryFacility = record.get("disc_facility");
-            String discoveryMethod = record.get("discoverymethod");
+    for (CSVRecord record : csvParser) {
+      String planetName = getStringFromCSVRecord("pl_name", record);
+      String hostName = getStringFromCSVRecord("hostname", record);
+      String discoveryYear = getStringFromCSVRecord("disc_year", record);
+      String discoveryFacility = getStringFromCSVRecord("disc_facility", record);
+      String discoveryMethod = getStringFromCSVRecord("discoverymethod", record);
 
-            Float orbitalPeriodDays = record.get("pl_orbper").isBlank() ? null :
-                    Float.parseFloat(record.get("pl_orbper"));
-            Float earthMass = record.get("pl_bmasse").isBlank() ? null :
-                    Float.parseFloat(record.get("pl_bmasse"));
-            Float earthRadius = record.get("pl_rade").isBlank() ? null :
-                    Float.parseFloat(record.get("pl_rade"));
-            Float systemDistanceInParsecs = record.get("sy_dist").isBlank() ? null :
-                    Float.parseFloat(record.get("sy_dist"));
-            Integer numStars = record.get("sy_snum").isBlank() ? null :
-                    Integer.parseInt(record.get("sy_snum"));
-            Integer numPlanets = record.get("sy_pnum").isBlank() ? null :
-                    Integer.parseInt(record.get("sy_pnum"));
+      Float orbitalPeriodDays = getFloatFromCSVRecord("pl_orbper", record);
+      Float earthMass = getFloatFromCSVRecord("pl_bmasse", record);
+      Float earthRadius = getFloatFromCSVRecord("pl_rade", record);
+      Float systemDistanceInParsecs = getFloatFromCSVRecord("sy_dist", record);
+      Integer numStars = getIntFromCSVRecord("sy_snum", record);
+      Integer numPlanets = getIntFromCSVRecord("sy_pnum", record);
 
-            Planet planet = new Planet(planetName, hostName, discoveryYear, discoveryFacility,
-                    discoveryMethod, orbitalPeriodDays, earthMass, earthRadius, true,
-                    systemDistanceInParsecs, numStars, numPlanets);
-            repo.save(planet);
-        }
+      Planet planet = new Planet(planetName, hostName, discoveryYear, discoveryFacility,
+          discoveryMethod, orbitalPeriodDays, earthMass, earthRadius, true, systemDistanceInParsecs,
+          numStars, numPlanets);
+      repo.save(planet);
     }
+  }
+
+  private static String getStringFromCSVRecord(String column, CSVRecord record) {
+    return record.get(column);
+  }
+
+  private static Float getFloatFromCSVRecord(String column, CSVRecord record) {
+    String value = record.get(column);
+    return value.isBlank() ? null : Float.parseFloat(value);
+  }
+
+  private static Integer getIntFromCSVRecord(String column, CSVRecord record) {
+    String value = record.get(column);
+    return value.isBlank() ? null : Integer.parseInt(value);
+  }
 }
